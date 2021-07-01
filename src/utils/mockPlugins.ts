b123f7185c67b4631627ebb4fbaf7f156514ce1b
Allen Tan <algtan.dev@gmail.com>
2021-07-01 16:17:09 -0700

    Create mock swap plugins

---
+++ src/utils/mockPlugins.ts
@@ -0,0 +1,15 @@
+interface PluginInfo {
+  pluginName: string
+}
+
+export const mockPlugins: PluginInfo[] = [
+  { pluginName: 'changelly' },
+  { pluginName: 'changenow' },
+  { pluginName: 'coinswitch' },
+  { pluginName: 'faast' },
+  { pluginName: 'foxexchange' },
+  { pluginName: 'godex' },
+  { pluginName: 'sideshift' },
+  { pluginName: 'switchain' },
+  { pluginName: 'totle' }
+]
