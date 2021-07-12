42ec0a4fa33bc804d8ab910538e68d0a1ac37139
Allen Tan <algtan.dev@gmail.com>
2021-07-12 15:43:22 -0700

    Add configuration for core to config file

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,11 +1,33 @@
 import { makeConfig } from 'cleaner-config'
-import { asNumber, asObject, asOptional, asString } from 'cleaners'
+import {
+  asBoolean,
+  asEither,
+  asNumber,
+  asObject,
+  asOptional,
+  asString
+} from 'cleaners'
+
+const asSwapPlugin = asObject({
+  apiKey: asOptional(asString),
+  secret: asOptional(asString),
+  affiliateId: asOptional(asString),
+  affiliateMargin: asOptional(asNumber),
+  partnerContract: asOptional(asString)
+})
+
+const asPlugin = asObject(asEither(asBoolean, asSwapPlugin))
 
 export const asConfig = asObject({
   dbFullpath: asOptional(asString, 'http://username:password@localhost:5984'),
   httpHost: asOptional(asString, '127.0.0.1'),
   httpPort: asOptional(asNumber, 8008),
-  timeBetweenCyclesInMinutes: asOptional(asNumber, 10)
+  timeBetweenCyclesInMinutes: asOptional(asNumber, 10),
+  apiKey: asOptional(asString, ''),
+  appId: asOptional(asString, ''),
+  username: asOptional(asString, ''),
+  password: asOptional(asString, ''),
+  plugins: asOptional(asPlugin, {})
 })
 
 export const config = makeConfig(asConfig, 'serverConfig.json')
