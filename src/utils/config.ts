c7ff9111693d7cc597e87b8f0c451f01fb7226c4
Allen Tan <algtan.dev@gmail.com>
2021-07-20 16:46:28 -0700

    fixup! Add configuration for core to config file

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -8,6 +8,17 @@ import {
   asString
 } from 'cleaners'
 
+const {
+  COUCH_HOSTNAME = '127.0.0.1',
+  COUCH_FULL_PATH = 'http://username:password@localhost:5984',
+  COUCH_PORT = '8008',
+  TIME_BETWEEN_CYCLES_IN_MINUTES = '10',
+  EDGE_API_KEY = '',
+  EDGE_APP_ID = '',
+  EDGE_USERNAME = '',
+  EDGE_PASSWORD = ''
+} = process.env
+
 const asSwapPlugin = asObject({
   apiKey: asOptional(asString),
   secret: asOptional(asString),
@@ -19,14 +30,17 @@ const asSwapPlugin = asObject({
 const asPlugin = asObject(asEither(asBoolean, asSwapPlugin))
 
 export const asConfig = asObject({
-  dbFullpath: asOptional(asString, 'http://username:password@localhost:5984'),
-  httpHost: asOptional(asString, '127.0.0.1'),
-  httpPort: asOptional(asNumber, 8008),
-  timeBetweenCyclesInMinutes: asOptional(asNumber, 10),
-  apiKey: asOptional(asString, ''),
-  appId: asOptional(asString, ''),
-  username: asOptional(asString, ''),
-  password: asOptional(asString, ''),
+  dbFullpath: asOptional(asString, COUCH_FULL_PATH),
+  httpHost: asOptional(asString, COUCH_HOSTNAME),
+  httpPort: asOptional(asNumber, parseInt(COUCH_PORT)),
+  timeBetweenCyclesInMinutes: asOptional(
+    asNumber,
+    parseInt(TIME_BETWEEN_CYCLES_IN_MINUTES)
+  ),
+  apiKey: asOptional(asString, EDGE_API_KEY),
+  appId: asOptional(asString, EDGE_APP_ID),
+  username: asOptional(asString, EDGE_USERNAME),
+  password: asOptional(asString, EDGE_PASSWORD),
   plugins: asOptional(asPlugin, {})
 })
 
